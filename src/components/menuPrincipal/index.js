356fec7a7fe894d54daa0c2179794258e59ddece
Javier Flores <jffc.dev@gmail.com>
2022-08-29 20:01:38 -0500

    feat(app): Add url button added to main page

--- src/components/menuPrincipal/index.js
+++ src/components/menuPrincipal/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import './index.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faDatabase, faHand, faList } from '@fortawesome/free-solid-svg-icons';
+import { faDatabase, faHand, faList, faAdd } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 function MenuPrincipal() {
@@ -17,6 +17,10 @@ function MenuPrincipal() {
                 <Link className='menu_principal__item' to={`/mantenimiento/list/db`}>
                     <FontAwesomeIcon className='faIcon' icon={faList} size="5x" inverse  />
                     <span>List DB</span>
+                </Link>
+                <Link className='menu_principal__item' to={`/mantenimiento/creacion/masiva`}>
+                    <FontAwesomeIcon className='faIcon' icon={faAdd} size="5x" inverse  />
+                    <span>Add URL</span>
                 </Link>
                 <Link className='menu_principal__item menu_principal__item__manual' to={`/mantenimiento/random/manual`}>
                     <FontAwesomeIcon className='faIcon' icon={faHand} size="5x" inverse  />
