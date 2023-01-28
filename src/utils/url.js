ea40a2a3ab1a6dc975c62931bdde0f3ad7d87ef4
Javier Flores <jffc.dev@gmail.com>
2023-01-28 11:00:37 -0500

    feat(App): updateArrayObject function added

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,71 +1,81 @@
-import axios from 'axios';
-import Swal from 'sweetalert2'
-import Constants from '../constants.js';
+import axios from "axios";
+import Swal from "sweetalert2";
+import Constants from "../constants.js";
 
 export const abrirUrlEspecifica = (cadena) => {
-    Object.assign(document.createElement('a'), {
-        target: '_blank',
-        href: cadena.url,
-    }).click();
-}
+  Object.assign(document.createElement("a"), {
+    target: "_blank",
+    href: cadena.url,
+  }).click();
+};
 
-export const reestablecerUrl = async(cadena, cadenas, setCadenas) => {
+export const reestablecerUrl = async (cadena, cadenas, setCadenas) => {
+  await Swal.fire({
+    title: "Reestablecer URL",
+    html:
+      `<span><b>Url actual:</b> <a href='${cadena.url}' target="_blank">Ver</a></span>` +
+      `<br/><textarea id="swal-input1" style="width: 100%"></textarea>`,
+    focusConfirm: false,
+    preConfirm: async () => {
+      const url = document.getElementById("swal-input1").value;
 
-    await Swal.fire({
-        title: 'Reestablecer URL',
-        html:
-            `<span><b>Url actual:</b> <a href='${cadena.url}' target="_blank">Ver</a></span>`+
-            `<br/><textarea id="swal-input1" style="width: 100%"></textarea>`,
-        focusConfirm: false,
-        preConfirm: async() => {
-            const url = document.getElementById('swal-input1').value
+      await axios
+        .patch(Constants.urlBackend + "/api/urls/add-reset/" + cadena._id, {
+          newUrl: url,
+        })
+        .then((response) => {
+          if (response.status === 201) {
+            const { data } = response;
 
-            await axios.patch(Constants.urlBackend+'/api/urls/add-reset/'+cadena._id, {
-                newUrl: url
-            })
-            .then((response) => {
-                if(response.status === 201){
+            if (data.status === 1) {
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Se reestableció correctamente la url",
+                showConfirmButton: false,
+                timer: 1500,
+              });
 
-                    const {data} = response;
+              updateArrayUrl(cadenas, setCadenas, cadena, data.rpta.url);
+            } else {
+              Swal.showValidationMessage(
+                `<i class="fa fa-info-circle"></i>${data.msg}`
+              );
+            }
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  });
+};
 
-                    if(data.status === 1){
+const updateArrayUrl = (cadenas, setCadenas, cadena, url) => {
+  let cadenasTemp = cadenas;
 
-                        Swal.fire({
-                            position: 'top-end',
-                            icon: 'success',
-                            title: 'Se reestableció correctamente la url',
-                            showConfirmButton: false,
-                            timer: 1500
-                        });
-                        
-                        updateArray(cadenas, setCadenas, cadena, data.rpta.url);
-      
-                    }else{
-                        Swal.showValidationMessage(
-                            `<i class="fa fa-info-circle"></i>${data.msg}`
-                        )
-                    }
-                }
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-            
-        }
-    });
-    
-}
+  const index = cadenasTemp.findIndex((object) => {
+    return object._id === cadena._id;
+  });
 
-const updateArray = (cadenas, setCadenas, cadena, url) => {
+  if (index !== -1) {
+    cadenasTemp[index].url = url;
+    setCadenas([]);
+    setCadenas(cadenasTemp);
+  }
+};
+
+
+export const updateArrayObject = (cadenas, setCadenas, urlOld, urlNew) => {
     let cadenasTemp = cadenas;
-
-    const index = cadenasTemp.findIndex(object => {
-        return object._id === cadena._id;
+  
+    const index = cadenasTemp.findIndex((object) => {
+      return object._id === urlOld._id;
     });
-    
+  
     if (index !== -1) {
-        cadenasTemp[index].url = url;
-        setCadenas([])
-        setCadenas(cadenasTemp)
+      cadenasTemp[index] = urlNew;
+      setCadenas([]);
+      setCadenas(cadenasTemp);
     }
-}
+  };
