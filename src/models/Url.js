08d330021ecf59e4dcb24d32dd8d588c20c1570f
Javier Flores <jffc.dev@gmail.com>
2023-01-30 11:10:42 -0500

    feat(App): currentTitle function added

--- src/models/Url.js
+++ src/models/Url.js
@@ -1,35 +1,35 @@
 export const Url = class {
-    constructor(_id, index, url, titles, dateCreated, resets) {
-        this._id = _id;
-        this.index = index;
-        this.url = url;
-        this.titles = titles;
-        this.dateCreated = dateCreated;
-        this.resets = resets;
+  constructor(_id, index, url, titles, dateCreated, resets) {
+    this._id = _id
+    this.index = index
+    this.url = url
+    this.titles = titles
+    this.dateCreated = dateCreated
+    this.resets = resets
+  }
+
+  get currentUrl() {
+    let maxReset = null
+
+    if (this.resets) {
+      const idReset = Math.max(...this.resets.map((reset) => reset._id))
+      maxReset = this.resets.find((reset) => {
+        return reset._id === idReset
+      })
     }
 
-    get currentUrl() {
+    return this.resets ? maxReset.url : this.url
+  }
 
-        let maxReset = null;
-
-        if(this.resets){
-            let idReset = Math.max(...this.resets.map(reset => reset._id))
-            maxReset = this.resets.find(reset => {
-                return reset._id === idReset;
-            });
-        }
-
-        return this.resets ? maxReset.url : this.url;
-
+  get currentTitle() {
+    let maxTitle = null
+    if (this.titles) {
+      const idReset = Math.max(...this.titles.map((reset) => reset._id))
+      maxTitle = this.titles.find((reset) => {
+        return reset._id === idReset
+      })
     }
-}
 
-export const UrlRandom = class {
-    constructor(_id, index, url, title, resets) {
-        this._id = _id;
-        this.index = index;
-        this.url = url;
-        this.title = title;
-        this.resets = resets;
-    }
+    return this.titles && maxTitle.title
+  }
 }
