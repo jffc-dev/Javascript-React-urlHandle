4535c5164a60e0e5dd14cb7b457a22d9da040214
Javier Flores <jffc.dev@gmail.com>
2023-02-03 08:22:36 -0500

    feat(App): Constants strcture changed

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,4 @@
-const CONSTANTS = {
-  urlBackend: 'http://localhost:3000',
-  OK_STATUS: 1,
-  FAIL_STATUS: 0
-}
-
-export default CONSTANTS
+export const API_URL_BASE = 'http://localhost:3000/'
+export const API_URL_URLS = 'api/url/'
+export const OK_STATUS = 1
+export const FAIL_STATUS = 0
