7a26aae05ccd1f6047da19b10a490c3d17396674
Javier Flores <jffc.dev@gmail.com>
2023-02-04 23:04:24 -0500

    feat(App): AleatorioUrlsBdPage Utils added

---
+++ src/utils/pages/AleatorioUrlsBdPageUtils.js
@@ -0,0 +1,77 @@
+import { OK_STATUS } from '../../constants'
+import { addTitleToUrl } from '../../services/urls/addTitleToUrl'
+import { getUrlsRandom } from '../../services/urls/getUrlsRandom'
+import { updateArrayObject } from '../url'
+
+export const OpenAdvancedGoogleSearch = (cadena) => {
+  const urlSearch = cadena.currentTitle || cadena.currentUrl
+  const urlGoogle = `https://www.google.com.pe/search?tbm=vid&hl=es-419&as_q=${encodeURI(
+    urlSearch.replace('&', '')
+  )}&as_epq=&as_oq=&as_eq=&lr=&as_qdr=all&as_sitesearch=&safe=images&tbs=dur%3Al`
+  Object.assign(document.createElement('a'), {
+    target: '_blank',
+    href: urlGoogle
+  }).click()
+}
+
+export const openCurrentLink = (cadenas, currentLink, setCurrentLink) => {
+  Object.assign(document.createElement('a'), {
+    target: '_blank',
+    href: cadenas.at(currentLink).url
+  }).click()
+  setCurrentLink(currentLink + 1)
+}
+
+export const handleGetUrlsRandom = async (
+  nroResultados,
+  setCadenas,
+  setCadenasFiltro,
+  setLinkCount,
+  setCurrentLink,
+  setmodalConfirmationRandomShow,
+  setToastAppProperties
+) => {
+  const { data, message, status } = await getUrlsRandom(nroResultados)
+
+  if (status === OK_STATUS) {
+    setCadenas(data)
+    setCadenasFiltro(data)
+    setLinkCount(data.length)
+    setCurrentLink(0)
+
+    setmodalConfirmationRandomShow(false)
+  } else {
+    setToastAppProperties({
+      title: 'ERROR',
+      message,
+      type: 'danger',
+      date: new Date()
+    })
+  }
+}
+
+export const PressOkModalLoadTitle = async (
+  cadena,
+  cadenaIndex,
+  cadenas,
+  setCadenas,
+  tFormTitle,
+  tFormReset,
+  setToastAppProperties,
+  setmodalLoadTitleShow
+) => {
+  const { data, message, status } = await addTitleToUrl(cadena._id, tFormTitle, cadenaIndex)
+  if (status === OK_STATUS) {
+    const urlOld = cadena
+    updateArrayObject(cadenas, setCadenas, urlOld, data)
+    tFormReset()
+    setmodalLoadTitleShow(false)
+  } else {
+    setToastAppProperties({
+      title: 'ERROR',
+      message,
+      type: 'danger',
+      date: new Date()
+    })
+  }
+}
