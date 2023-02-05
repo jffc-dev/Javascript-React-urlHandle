9576aea776fd500f3692c6021aa749d7a15f5082
Javier Flores <jffc.dev@gmail.com>
2023-02-04 23:05:37 -0500

    fix(App): getUrlsRandom service response fixed

--- src/services/urls/getUrlsRandom.js
+++ src/services/urls/getUrlsRandom.js
@@ -2,29 +2,48 @@
 import axios from 'axios'
 
 // Owns
-import CONSTANTS from '../../constants'
+import { API_URL_BASE, API_URL_URLS, OK_STATUS } from '../../constants'
+import { ServiceResponse } from '../../models/Response'
 import { Url } from '../../models/Url'
 
 export const getUrlsRandom = async (nroResultados) => {
-  const cadenasClase = []
+  const cadenasClase = await axios
+    .get(API_URL_BASE + API_URL_URLS + 'random/' + nroResultados)
+    .then((response) => {
+      const { data, message, status } = response.data
 
-  await axios.get(CONSTANTS.urlBackend + '/api/url/random/' + nroResultados).then((res) => {
-    const datos = res.data.urls
+      if (status === OK_STATUS) {
+        const datosClass = data.map((cadena, index) => {
+          return new Url(
+            cadena._id,
+            index + 1,
+            cadena.url,
+            cadena.titles,
+            cadena.audi_createdDate,
+            cadena.resets
+          )
+        })
 
-    datos.map((cadena, index) => {
-      cadenasClase.push(
-        new Url(
-          cadena._id,
-          index + 1,
-          cadena.url,
-          cadena.titles,
-          cadena.audi_createdDate,
-          cadena.resets
-        )
-      )
-      return null
+        return new ServiceResponse({
+          status,
+          message,
+          data: datosClass
+        })
+      } else {
+        return new ServiceResponse({
+          status,
+          message: 'Service error. ' + message,
+          data: null
+        })
+      }
     })
-  })
+    .catch((error) => {
+      return new ServiceResponse({
+        status: 0,
+        message: 'An error occurred reading the service response. ' + error,
+        data: null
+      })
+    })
 
   return cadenasClase
 }
