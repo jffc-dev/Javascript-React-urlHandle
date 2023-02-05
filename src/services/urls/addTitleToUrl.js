7fae9f970298565157891199446262d0ab2a9102
Javier Flores <jffc.dev@gmail.com>
2023-02-05 01:35:33 -0500

    fix(App): addTitleToUrl response fixed

--- src/services/urls/addTitleToUrl.js
+++ src/services/urls/addTitleToUrl.js
@@ -2,26 +2,45 @@
 import axios from 'axios'
 
 // Owns
-import CONSTANTS from '../../constants'
+import { API_URL_BASE, API_URL_URLS, OK_STATUS } from '../../constants'
+import { ServiceResponse } from '../../models/Response'
+import { Url } from '../../models/Url'
 
-export const addTitleToUrl = async (idUrl, title) => {
+export const addTitleToUrl = async (cadenaId, title, cadenaIndex) => {
   const rpta = await axios
-    .patch(CONSTANTS.urlBackend + '/api/urls/add-title/' + idUrl, {
+    .patch(API_URL_BASE + API_URL_URLS + 'add-title/' + cadenaId, {
       title
     })
     .then((response) => {
-      if (response.status === 201) {
-        return response.data
+      const { data, message, status } = response.data
+      if (status === OK_STATUS) {
+        const urlClass = new Url(
+          data._id,
+          cadenaIndex,
+          data.url,
+          data.titles,
+          data.audi_createdDate,
+          data.resets
+        )
+        return new ServiceResponse({
+          status,
+          message,
+          data: urlClass
+        })
+      } else {
+        return new ServiceResponse({
+          status,
+          message: 'Service error. ' + message,
+          data: null
+        })
       }
     })
     .catch((error) => {
-      console.log(error)
-
-      return {
+      return new ServiceResponse({
         status: 0,
-        msg: 'Se produjo un error al consultar el servicio.',
-        rpta: null
-      }
+        message: 'An error occurred reading the service response. ' + error,
+        data: null
+      })
     })
 
   return rpta
