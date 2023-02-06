5a404738fa806ab4598165609cca2fa876117728
Javier Flores <jffc.dev@gmail.com>
2023-02-06 00:40:43 -0500

    feat(App): Components param changed

--- src/components/AleatorioUrlsBd/AleatorioUrlsBdModal/AleatorioUrlsBdModalTabResets/AleatorioUrlsBdModalTabResets.js
+++ src/components/AleatorioUrlsBd/AleatorioUrlsBdModal/AleatorioUrlsBdModalTabResets/AleatorioUrlsBdModalTabResets.js
@@ -1,36 +1,45 @@
-import { faClipboard } from '@fortawesome/free-solid-svg-icons';
-import GeneralTableButton from '../../../General/TableButton/TableButton';
+import React from 'react'
+import { faClipboard } from '@fortawesome/free-solid-svg-icons'
+import GeneralTableButton from '../../../General/TableButton/TableButton'
 
-const AleatorioUrlsBdModalTabTitles = ({resets}) => {
-    return(
-        <div className="col-12">
-            <table>
-                <thead>
-                    <tr>
-                        <th>URL</th>
-                        <th style={{width: '20%'}}>Opciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {resets.length ? resets.map((reset, index)=>
-                    (
-                        <tr key={index}>
-                            <td className='url'>
-                                {reset.url}
-                            </td>
-                            <td style={{width: '10%'}}>
-                                <GeneralTableButton faIcon={faClipboard} msgTooltip={"Ver"} 
-                                    action={()=>{navigator.clipboard.writeText(reset.url)}}
-                                ></GeneralTableButton>
-                            </td>
-                        </tr>
-                    )) : 
-                    <tr><td colSpan={2} style={{textAlign: 'center'}}>No se cargaron registros</td></tr>
-                }
-                </tbody>
-            </table>
-        </div>
-    );
+const AleatorioUrlsBdModalTabTitles = ({ cadena }) => {
+  return (
+    <div className="col-12">
+      <table>
+        <thead>
+          <tr>
+            <th>URL</th>
+            <th style={{ width: '20%' }}>Opciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cadena &&
+            cadena.resets &&
+            (cadena.resets.length ? (
+              cadena.resets.map((reset, index) => (
+                <tr key={index}>
+                  <td className="url">{reset.url}</td>
+                  <td style={{ width: '10%' }}>
+                    <GeneralTableButton
+                      faIcon={faClipboard}
+                      msgTooltip={'Ver'}
+                      action={() => {
+                        navigator.clipboard.writeText(reset.url)
+                      }}></GeneralTableButton>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={2} style={{ textAlign: 'center' }}>
+                  No se cargaron registros
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default AleatorioUrlsBdModalTabTitles
--- src/components/AleatorioUrlsBd/AleatorioUrlsBdModal/AleatorioUrlsBdModalTabTitles/AleatorioUrlsBdModalTabTitles.js
+++ src/components/AleatorioUrlsBd/AleatorioUrlsBdModal/AleatorioUrlsBdModalTabTitles/AleatorioUrlsBdModalTabTitles.js
@@ -1,77 +1,103 @@
-import { faClipboard, faSquarePlus } from '@fortawesome/free-solid-svg-icons';
-import { useForm } from '../../../../hooks/useForm';
-import { addTitleToUrl } from '../../../../services/urls/addTitleToUrl';
-import GeneralTableButton from '../../../General/TableButton/TableButton';
-import FormButtonComponent from '../../../General/FormButtonComponent/FormButtonComponent';
+import React, { useContext } from 'react'
+import { faClipboard, faSquarePlus } from '@fortawesome/free-solid-svg-icons'
+import { useForm } from '../../../../hooks/useForm'
+import { addTitleToUrl } from '../../../../services/urls/addTitleToUrl'
+import GeneralTableButton from '../../../General/TableButton/TableButton'
+import FormButtonComponent from '../../../General/FormButtonComponent/FormButtonComponent'
+import { AppContext } from '../../../../utils/AppContext'
+import { OK_STATUS } from '../../../../constants'
+import { updateArrayObject } from '../../../../utils/url'
 
-const AleatorioUrlsBdModalTabTitle = ({titles, setTitles, cadenaId}) => {
+const AleatorioUrlsBdModalTabTitle = ({ cadena, setCadena, cadenas, setCadenas, cadenaIndex }) => {
+  // Title form
+  const [{ tFormTitle }, tFormHandleInputChange, tFormReset] = useForm({
+    tFormTitle: '',
+    initial: true
+  })
 
-    // Title form
-    const [{tFormTitle}, tFormHandleInputChange, tFormReset] = useForm({
-        tFormTitle: '',
-        initial: true
-    });
+  const { setToastAppProperties } = useContext(AppContext)
 
-    const handleTitleFormSubmit = async(e) => {
+  const handleTitleFormSubmit = async (e) => {
+    e.preventDefault()
+    const { data, message, status } = await addTitleToUrl(cadena._id, tFormTitle, cadenaIndex)
 
-        e.preventDefault();
-        
-        const rpta = await addTitleToUrl(cadenaId, tFormTitle);
-
-        if(rpta.status === 1){
-            const {rpta:inserted} = rpta;
-            let titlesNew = [...titles,inserted];
-            tFormReset();
-            setTitles(titlesNew);
-        }
-        
+    if (status === OK_STATUS) {
+      const urlOld = cadena
+      updateArrayObject(cadenas, setCadenas, urlOld, data)
+      tFormReset()
+      setCadena(data)
+    } else {
+      setToastAppProperties({
+        title: 'ERROR',
+        message,
+        type: 'danger',
+        date: new Date()
+      })
     }
+  }
 
-    return(
-        <div>
-            <form onSubmit={handleTitleFormSubmit}>
-                <div className="form-group row">
-                    <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Título</label>
-                    <div className="col-sm-10">
-                        <div className='inputModal__container'>
-                            <input type="text" className="form-control" value={tFormTitle} onChange={tFormHandleInputChange} autoComplete="off" name="tFormTitle"/>
-                        </div>
-                    </div>
-                </div>
-                <div className="form-group col-12 inputModal__containerButtons">
-                    <FormButtonComponent text={"Agregar"} faIcon={faSquarePlus}></FormButtonComponent>
-                </div>
-            </form>
-            
-            <div className="col-12">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Título</th>
-                            <th style={{width: '20%'}}>Opciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {titles.length ? titles.map((title, index)=>
-                        (
-                            <tr key={index}>
-                                <td className='url'>
-                                    {title.title}
-                                </td>
-                                <td style={{width: '10%'}}>
-                                    <GeneralTableButton faIcon={faClipboard} msgTooltip={"Ver"} 
-                                        action={()=>{navigator.clipboard.writeText(title.title)}}
-                                    ></GeneralTableButton>
-                                </td>
-                            </tr>
-                        )) : 
-                        <tr><td colSpan={2} style={{textAlign: 'center'}}>No se cargaron registros</td></tr>
-                    }
-                    </tbody>
-                </table>
+  return (
+    <div>
+      <form onSubmit={handleTitleFormSubmit}>
+        <div className="form-group row">
+          <label htmlFor="staticEmail" className="col-sm-2 col-form-label">
+            Título
+          </label>
+          <div className="col-sm-10">
+            <div className="inputModal__container">
+              <input
+                type="text"
+                className="form-control"
+                value={tFormTitle}
+                onChange={tFormHandleInputChange}
+                autoComplete="off"
+                name="tFormTitle"
+              />
             </div>
+          </div>
         </div>
-    )
+        <div className="form-group col-12 inputModal__containerButtons">
+          <FormButtonComponent text={'Agregar'} faIcon={faSquarePlus}></FormButtonComponent>
+        </div>
+      </form>
+
+      <div className="col-12">
+        <table>
+          <thead>
+            <tr>
+              <th>Título</th>
+              <th style={{ width: '20%' }}>Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cadena &&
+              cadena.titles &&
+              (cadena.titles.length ? (
+                cadena.titles.map((title, index) => (
+                  <tr key={index}>
+                    <td className="url">{title.title}</td>
+                    <td style={{ width: '10%' }}>
+                      <GeneralTableButton
+                        faIcon={faClipboard}
+                        msgTooltip={'Ver'}
+                        action={() => {
+                          navigator.clipboard.writeText(title.title)
+                        }}></GeneralTableButton>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan={2} style={{ textAlign: 'center' }}>
+                    No se cargaron registros
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
 
-export default AleatorioUrlsBdModalTabTitle;
+export default AleatorioUrlsBdModalTabTitle
