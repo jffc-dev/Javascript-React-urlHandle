9422294e085c75ab9faf7bebc7ba8dd077532175
Javier Flores <jffc.dev@gmail.com>
2023-02-06 00:37:59 -0500

    fix(App): Code formatted

--- src/components/Filtros/KeyPressFilterComponent/KeyPressFilterComponent.js
+++ src/components/Filtros/KeyPressFilterComponent/KeyPressFilterComponent.js
@@ -3,26 +3,31 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useState } from 'react'
 import './KeyPressFilterComponent.css'
 
-export const KeyPressFilterComponent = ({cadenas, setCadenasFiltro}) => {
+export const KeyPressFilterComponent = ({ cadenas, setCadenasFiltro }) => {
+  const [busqueda, setBusqueda] = useState('')
 
-    const [busqueda, setBusqueda] = useState("")
-
-    const onChangeSearch = (event) => {
-        setBusqueda(event.target.value)
-        setCadenasFiltro(cadenas.filter(cadena=>cadena.url.toLowerCase().includes(event.target.value.toLowerCase())))
-    }
-
-    const clearSearch = () => {
-        setBusqueda("")
-        onChangeSearch({
-            target: {value: ""}
-        })
-    }
-
-    return (
-        <div className='inputKeyPress__container'>
-            <input value={busqueda} onChange={onChangeSearch} className='inputKeyPress__filter'/>
-            <button className='inputKeyPress__button' onClick={clearSearch}><FontAwesomeIcon className='faIcon' icon={faX} inverse  /></button>
-        </div>
+  const onChangeSearch = (event) => {
+    setBusqueda(event.target.value)
+    setCadenasFiltro(
+      cadenas.filter((cadena) =>
+        cadena.url.toLowerCase().includes(event.target.value.toLowerCase())
+      )
     )
+  }
+
+  const clearSearch = () => {
+    setBusqueda('')
+    onChangeSearch({
+      target: { value: '' }
+    })
+  }
+
+  return (
+    <div className="inputKeyPress__container">
+      <input value={busqueda} onChange={onChangeSearch} className="inputKeyPress__filter" />
+      <button className="inputKeyPress__button" onClick={clearSearch}>
+        <FontAwesomeIcon className="faIcon" icon={faX} inverse />
+      </button>
+    </div>
+  )
 }
