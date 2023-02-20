dbec1013c970e1ed0a101a67d176c2b6de704234
Javier Flores <jffc.dev@gmail.com>
2023-02-19 23:42:49 -0500

    feat(App): TemplateParser and sticky cols applied

--- src/components/general/TableComponent/TableComponent.js
+++ src/components/general/TableComponent/TableComponent.js
@@ -2,43 +2,44 @@ import React from 'react'
 import { Table } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import GeneralTableButton from '../../../components/General/TableButton/TableButton'
+import { TemplateParser } from '../../../models/TemplateParser'
+import './TableComponent.css'
 
-const TableComponent = ({ data, heads, actions }) => {
-  // const action1Handle = (item) => {
-  //   action1Function(item)
-  // }
+const TableComponent = ({ data, setData, heads, actions }) => {
   return (
     <Table striped responsive>
       <thead className="table-dark">
         <tr>
+          {actions && <th className="sticky-col table-head-center">Acciones</th>}
           {Object.entries(heads).map(([key, value]) => (
-            <th key={key} style={{ width: value.width + '%' }}>
+            <th key={key} className="table-head-center" style={{ width: value.width + 'px' }}>
               {value.name}
             </th>
           ))}
-          {actions && <th>Acciones</th>}
         </tr>
       </thead>
       <tbody>
         {data.map((item, index) => {
           return (
             <tr key={index}>
-              {Object.entries(heads).map(([key, value]) => (
-                <th key={key} style={{ width: value.width + '%' }}>
-                  {item[key]}
-                </th>
-              ))}
-
-              <th>
+              <th className="sticky-col sticky-col-tbody">
                 {actions &&
                   actions.map((action, indexAct) => (
                     <GeneralTableButton
                       key={'action_' + index + '_' + indexAct}
                       faIcon={action.actionIcon}
                       msgTooltip={action.actionText}
-                      action={(_) => action.actionFunction(item)}></GeneralTableButton>
+                      action={(_) =>
+                        action.actionFunction(item, data, setData)
+                      }></GeneralTableButton>
                   ))}
               </th>
+
+              {Object.entries(heads).map(([key, value]) => (
+                <th key={key} style={{ width: value.width + 'px' }}>
+                  {TemplateParser(item[key], value.type)}
+                </th>
+              ))}
             </tr>
           )
         })}
