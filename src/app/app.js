fd12655a34315f250911d91f335358aa9d8ca450
Javier Flores <jffc.dev@gmail.com>
2023-03-01 21:28:09 -0500

    feat(App): AppRouter component referenced

--- src/app/app.js
+++ src/app/app.js
@@ -1,19 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React, { useState } from 'react'
 
-import NavbarComponent from '../components/General/NavbarComponent/NavbarComponent'
-import AleatorioUrlsManualPage from '../pages/AleatorioUrlsManualPage/AleatorioUrlsManualPage'
-import CreacionUrlsMasivaPage from '../pages/CreacionUrlsMasivaPage/CreacionUrlsMasivaPage'
-import AleatorioUrlsBdPage from '../pages/AleatorioUrlsBdPage/AleatorioUrlsBdPage'
-import ListaUrlsBdPage from '../pages/ListaUrlsBdPage/ListaUrlsBdPage'
-import MenuPrincipalPage from '../pages/MenuPrincipalPage/MenuPrincipalPage'
-import LoaderFullScreenComponent from '../components/General/LoaderFullScreenComponent/LoaderFullScreenComponent'
-
 import { AppContext } from '../utils/AppContext'
-import ToastContainerComponent from '../components/General/ToastContainerComponent/ToastContainerComponent'
-import ListaUrlsBdPaginationPage from '../pages/ListaUrlsBdPaginationPage/ListaUrlsBdPaginationPage'
+import AppRouter from './AppRouter'
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false)
   const [toastAppProperties, setToastAppProperties] = useState({
     title: '',
     message: '',
@@ -24,32 +15,16 @@ const App = () => {
 
   const context = {
     setShowLoaderApp,
-    setToastAppProperties
+    setToastAppProperties,
+    setDarkMode
   }
 
   return (
     <AppContext.Provider value={context}>
-      <BrowserRouter>
-        <ToastContainerComponent toastAppProperties={toastAppProperties}></ToastContainerComponent>
-        <LoaderFullScreenComponent show={showLoaderApp}></LoaderFullScreenComponent>
-        <NavbarComponent></NavbarComponent>
-        <Routes>
-          <Route path="/" element={<MenuPrincipalPage />} />
-          <Route path="mantenimiento">
-            <Route path="creacion">
-              <Route path="masiva" element={<CreacionUrlsMasivaPage />} />
-            </Route>
-            <Route path="random">
-              <Route path="db" element={<AleatorioUrlsBdPage />} />
-              <Route path="manual" element={<AleatorioUrlsManualPage />} />
-            </Route>
-            <Route path="list">
-              <Route path="db" element={<ListaUrlsBdPage />} />
-              <Route path="dbv2" element={<ListaUrlsBdPaginationPage />} />
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <AppRouter
+        darkMode={darkMode}
+        toastAppProperties={toastAppProperties}
+        showLoaderApp={showLoaderApp}></AppRouter>
     </AppContext.Provider>
   )
 }
