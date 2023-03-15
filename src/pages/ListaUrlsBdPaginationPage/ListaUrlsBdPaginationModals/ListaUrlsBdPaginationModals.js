82adc47786bcfcaa6a95c8227cae6063e4bfafb4
Javier Flores <jffc.dev@gmail.com>
2023-03-15 03:54:58 -0500

    feat(App): ConfirmationModalComponent added in ListaUrlsBdPaginationModals component

--- src/pages/ListaUrlsBdPaginationPage/ListaUrlsBdPaginationModals/ListaUrlsBdPaginationModals.js
+++ src/pages/ListaUrlsBdPaginationPage/ListaUrlsBdPaginationModals/ListaUrlsBdPaginationModals.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import AleatorioUrlsBdModalReset from '../../../components/AleatorioUrlsBd/AleatorioUrlsBdModal/AleatorioUrlsBdModalReset/AleatorioUrlsBdModalReset'
+import ConfirmationModalComponent from '../../../components/General/ConfirmationModal/ConfirmationModalComponent'
 
-const ListaUrlsBdPaginationModals = ({ AleatorioUrlsBdModalResetObjects }) => {
+const ListaUrlsBdPaginationModals = ({
+  AleatorioUrlsBdModalResetObjects,
+  ConfirmationModalDeleteObjects
+}) => {
   const { modalResetShow, setmodalResetShow } = AleatorioUrlsBdModalResetObjects
+  const { modalConfirmationDeleteShow, setmodalConfirmationDeleteShow, PressOkModalDelete } =
+    ConfirmationModalDeleteObjects
   return (
     <div>
       <AleatorioUrlsBdModalReset
@@ -12,6 +18,18 @@ const ListaUrlsBdPaginationModals = ({ AleatorioUrlsBdModalResetObjects }) => {
         modalOkBtnAction={''}
         formData={{ newUrl: '' }}
         modalCancelBtnAction={() => {}}></AleatorioUrlsBdModalReset>
+
+      {/* Delete url confirmation model */}
+      <ConfirmationModalComponent
+        modalShow={modalConfirmationDeleteShow}
+        setModalShow={setmodalConfirmationDeleteShow}
+        modalHeader="URL Delete"
+        modalOkBtnText="Yes, i want."
+        modalCloseBtnText="Cancel"
+        modalOkBtnAction={PressOkModalDelete}
+        modalBody={
+          <div>Are you sure that you want to delete this URL?</div>
+        }></ConfirmationModalComponent>
     </div>
   )
 }
