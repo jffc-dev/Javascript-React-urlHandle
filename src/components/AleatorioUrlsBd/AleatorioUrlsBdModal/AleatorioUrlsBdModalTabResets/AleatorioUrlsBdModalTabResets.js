fe5214f9d6ed6de055dbf308641dddbdad3e3fb4
Javier Flores <jffc.dev@gmail.com>
2023-03-16 23:12:33 -0500

    fix(App): Resets modal validations issue fixed

--- src/components/AleatorioUrlsBd/AleatorioUrlsBdModal/AleatorioUrlsBdModalTabResets/AleatorioUrlsBdModalTabResets.js
+++ src/components/AleatorioUrlsBd/AleatorioUrlsBdModal/AleatorioUrlsBdModalTabResets/AleatorioUrlsBdModalTabResets.js
@@ -9,33 +9,31 @@ const AleatorioUrlsBdModalTabTitles = ({ cadena }) => {
         <thead>
           <tr>
             <th>URL</th>
-            <th style={{ width: '20%' }}>Opciones</th>
+            <th style={{ width: '20%' }}>Options</th>
           </tr>
         </thead>
         <tbody>
-          {cadena &&
-            cadena.resets &&
-            (cadena.resets.length ? (
-              cadena.resets.map((reset, index) => (
-                <tr key={index}>
-                  <td className="url">{reset.url}</td>
-                  <td style={{ width: '10%' }}>
-                    <GeneralTableButton
-                      faIcon={faClipboard}
-                      msgTooltip={'Ver'}
-                      action={() => {
-                        navigator.clipboard.writeText(reset.url)
-                      }}></GeneralTableButton>
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan={2} style={{ textAlign: 'center' }}>
-                  No se cargaron registros
+          {cadena?.resets?.length ? (
+            cadena.resets.map((reset, index) => (
+              <tr key={index}>
+                <td className="url">{reset.url}</td>
+                <td style={{ width: '10%' }}>
+                  <GeneralTableButton
+                    faIcon={faClipboard}
+                    msgTooltip={'Ver'}
+                    action={() => {
+                      navigator.clipboard.writeText(reset.url)
+                    }}></GeneralTableButton>
                 </td>
               </tr>
-            ))}
+            ))
+          ) : (
+            <tr>
+              <td colSpan={2} style={{ textAlign: 'center' }}>
+                No se cargaron registros
+              </td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
