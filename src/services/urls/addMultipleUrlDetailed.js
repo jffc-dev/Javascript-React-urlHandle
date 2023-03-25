b45b1c1bd4b8b36ad8808483c398cbdf46d51127
Javier Flores <jffc.dev@gmail.com>
2023-03-25 01:56:24 -0500

    feat(App): addMultipleUrlDetailed Service added

---
+++ src/services/urls/addMultipleUrlDetailed.js
@@ -0,0 +1,48 @@
+// Thirds
+import axios from 'axios'
+
+// Owns
+import { API_URL_BASE, API_URL_URLS, OK_STATUS } from '../../constants'
+import { ServiceResponse } from '../../models/Response'
+
+export const addMultipleUrlDetailedService = async (urls) => {
+  const resp = await axios
+    .post(
+      API_URL_BASE + API_URL_URLS + 'multiple-detailed/',
+      {
+        urls
+      },
+      {
+        validateStatus: function (status) {
+          return status === 500
+        }
+      }
+    )
+    .then((response) => {
+      const { data, message, status } = response.data
+
+      if (status === OK_STATUS) {
+        return new ServiceResponse({
+          status,
+          message,
+          data
+        })
+      } else {
+        return new ServiceResponse({
+          status,
+          message: 'Service error. ' + message,
+          data: null
+        })
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+      return new ServiceResponse({
+        status: 0,
+        message: 'An error occurred reading the service response. ' + error,
+        data: null
+      })
+    })
+
+  return resp
+}
